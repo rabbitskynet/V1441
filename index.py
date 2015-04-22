fd7360af81de8b63e376ec113ef99900fadc3f0c
Jack Smith <rabbitskynet@gmail.com>
2015-04-22 17:28:38 +0300

    add footer and header

--- index.py
+++ index.py
@@ -13,19 +13,22 @@ sql_debug(True)
 def index():
 	automarks = select(c.automark for c  in Car)
 	users = select(u for u in User)
-	return render_template("index.html", automarks=automarks, users=users)
+	title = u'Главная'
+	return render_template("index.html", title=title, automarks=automarks, users=users)
 
 @app.route('/automark/<name>')
 @db_session
 def get_automark(name):
 	cars = select((c.model, count(c)) for c in Car if c.automark == name)
-	return render_template("automark.html", name=name, cars=cars)
+	title = name
+	return render_template("automark.html", title=title, name=name, cars=cars)
 
 @app.route('/user/<name>')
 @db_session
 def get_user(name):
     user = User[name]
-    return render_template("user.html", user=user)
+    title =  "User: " + name
+    return render_template("user.html", title=title, user=user)
 
 @app.route('/update', methods=['POST'])
 @db_session

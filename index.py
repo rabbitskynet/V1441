208abed7f97c2d069cb837856e51779f8a9ec7b5
Jack Smith <rabbitskynet@gmail.com>
2015-06-03 15:19:56 +0300

    made some changes

--- index.py
+++ index.py
@@ -3,16 +3,16 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 import json
 from models import *
 from js import *
+import sys
 
 app = Flask(__name__)
 sql_debug(True)
-
 
 @app.route('/')
 @db_session
 def index():
     title = u'Главная'
-    return render_template("model_single.html", current = "main", title=title, menuuser = getuser())
+    return render_template("model_single.html", current = "main", title=title)
 
 @app.route('/automarks')
 @db_session
@@ -20,54 +20,52 @@ def get_json():
     automarks = Automark.select()
     return to_json(db, automarks, include=[Automark.models])
 
-@app.route('/login', methods=['GET', 'POST'])
-@app.route('/login?<addadv>', methods=['GET'])
+@app.route('/get-current-user')
 @db_session
-def show_login_form(addadv=''):
+def get_json_user():
+    return to_json(db, getuser())
+
+@app.route('/login', methods=['GET', 'POST'])
+@db_session
+def show_login_form():
     if request.method == 'POST':
-        print request.form['username']
-        print request.form['password']
-        user = User.get(login=request.form['username'],password = request.form['password'])
-        if user:
-            setuser(user)
+        userfound = User.get(login=request.form['username'],password = request.form['password'])
+        if userfound:
+            setuser(userfound)
             url = url_for('index')
             return redirect(url)
         else:
-            return render_template("login.html", current = "login",status=1, title="Login form", menuuser = getuser())
+            return render_template("login.html", current = "login",status=1, title="Logi2n form")
+            redirect(redirect_url())
     else:
         if getuser():
             url = url_for('index')
             return redirect(url)
-        if addadv:
-            return render_template("login.html", current = "login",status=2, title="Logi2n form", menuuser = getuser())
         else:
-            return render_template("login.html", current = "login",status=-1, title="Login form", menuuser = getuser())
+			return render_template("login.html", current = "login",status=-1, title="Login form")
 
 
 @app.route('/addadv', methods=['GET', 'POST'])
 @db_session
 def add_adv_form():
     if request.method == 'POST':
-        print request.form['username']
-        print request.form['password']
         user = User.get(login=request.form['username'],password = request.form['password'])
         if user:
             setuser(user)
-            return render_template("login.html", current = "login",status=0, title="Login form", menuuser = getuser())
+            return render_template("login.html", current = "login",status=0, title="Login form")
         else:
-            return render_template("login.html", current = "login",status=1, title="Login form", menuuser = getuser())
+            return render_template("login.html", current = "login",status=1, title="Login form")
     else:
         if getuser():
             automarks = Automark.select()
-            return render_template("addadv.html", current = "addadv",automarks=automarks, title="Add adv form", menuuser = getuser())
+            return render_template("addadv.html", current = "addadv",automarks=automarks, title="Add adv form")
         else:
-            url = url_for('show_login_form',addadv='1')
+            url = url_for('show_login_form')
             return redirect(url)
 
 @app.route('/get-adv-bymodel/<model>')
 @db_session
 def get_model_json(model):
-    #print model
     advs = select(a for a in Adv if a.car.model.name == model)
     return to_json(db, {'advs': advs})
 
@@ -78,13 +76,6 @@ def logoutuser():
     logout()
     url = url_for('show_login_form')
     return redirect(url)
-
-@app.route('/user/')
-@db_session
-def get_all_users():
-    users= select(u for u in User)
-    title = u'Пользователи'
-    return render_template("users.html", current = "users",title=title, users=users, menuuser = getuser())
 
 def getuser():
     if 'user' in session.keys():
